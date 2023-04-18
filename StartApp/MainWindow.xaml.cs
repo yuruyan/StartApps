2940d2061efb5253a50c46ad29d1a017acebc67a
Luxiu123 <klt5466@163.com>
2023-04-18 15:48:56 +0800

    Update style

--- StartApp/MainWindow.xaml.cs
+++ StartApp/MainWindow.xaml.cs
@@ -1,11 +1,23 @@
 ﻿using CommonUITools.Controls;
+using CommonUITools.Model;
+using CommonUITools.Themes;
 
 namespace StartApp;
 
 public partial class MainWindow : BaseWindow {
     public MainWindow() {
         InitializeComponent();
+        UpdateTheme(SystemColorsHelper.CurrentSystemTheme);
+        SystemColorsHelper.SystemThemeChanged += (_, theme) => UpdateTheme(theme);
         // 初始化
         ContentFrame.Content = new MainView();
+    }
+
+    private void UpdateTheme(ThemeMode theme) {
+        if (theme == ThemeMode.Light) {
+            ThemeManager.SwitchToLightTheme();
+        } else {
+            ThemeManager.SwitchToDarkTheme();
+        }
     }
 }
