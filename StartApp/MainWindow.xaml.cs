0c846ff638a718e81b743076145d0d1463a49961
Luxiu123 <klt5466@163.com>
2023-04-28 20:53:41 +0800

    Update Window theme

--- StartApp/MainWindow.xaml.cs
+++ StartApp/MainWindow.xaml.cs
@@ -1,23 +1,12 @@
-﻿using CommonUITools.Controls;
-using CommonUITools.Model;
-using CommonUITools.Themes;
-
-namespace StartApp;
+﻿namespace StartApp;
 
 public partial class MainWindow : BaseWindow {
     public MainWindow() {
         InitializeComponent();
-        UpdateTheme(SystemColorsHelper.CurrentSystemTheme);
-        SystemColorsHelper.SystemThemeChanged += (_, theme) => UpdateTheme(theme);
-        // 初始化
-        ContentFrame.Content = new MainView();
-    }
-
-    private void UpdateTheme(ThemeMode theme) {
-        if (theme == ThemeMode.Light) {
-            ThemeManager.SwitchToLightTheme();
-        } else {
-            ThemeManager.SwitchToDarkTheme();
-        }
+        this.SetLoadedOnceEventHandler((_, _) => {
+            WindowHelper.ApplyOptimalThemeStyle(this);
+            // 初始化
+            ContentFrame.Content = new MainView();
+        });
     }
 }
