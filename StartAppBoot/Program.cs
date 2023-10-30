d8aa5937843fbe7be3bd9b912e92be5e632be646
Luxiu123 <klt5466@163.com>
2023-10-30 22:38:07 +0800

    Refactor StartAppBoot

--- StartAppBoot/Program.cs
+++ StartAppBoot/Program.cs
@@ -1,9 +1,6 @@
-﻿// 如果有管理员权限运行的任务，那么此程序是以管理员身份运行的
-using CommonTools.Utils;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using NLog;
 using Shared.Model;
-using StartAppBoot;
 using System.Diagnostics;
 
 Logger Logger = LogManager.GetCurrentClassLogger();
@@ -19,68 +16,34 @@ if (!File.Exists(configurationFile)) {
     Logger.Error($"文件 '{configurationFile}' 不存在");
     return;
 }
-var tasks = JsonConvert.DeserializeObject<IEnumerable<AppTaskPO>>(File.ReadAllText(configurationFile));
+var appTasks = JsonConvert.DeserializeObject<IList<AppTaskPO>>(
+    File.ReadAllText(configurationFile)
+);
 // 解析失败
-if (tasks is null) {
+if (appTasks is null) {
     return;
 }
 // 筛选
-tasks = tasks.Where(task => task.IsEnabled);
-var normalTasks = tasks.Where(task => !task.RunAsAdministrator).ToList();
-var adminTasks = tasks.Where(task => task.RunAsAdministrator).ToList();
+appTasks = appTasks.Where(task => task.IsEnabled).ToList();
+var runningTasks = new Task[appTasks.Count];
 
-var normalRunningTasks = new Task[normalTasks.Count];
-var adminRunningTasks = new Task[adminTasks.Count];
-
-// 执行
-RunTask(normalTasks, normalRunningTasks, false);
-RunTask(adminTasks, adminRunningTasks, true);
-
-void RunTask(IEnumerable<AppTaskPO> tasks, Task[] runningTasks, bool runAsAdmin) {
-    if (runningTasks.Length == 0) {
-        return;
-    }
-    int i = 0;
-    var shellProcessStartInfo = new ProcessStartInfo {
-        FileName = "powershell.exe",
-        RedirectStandardInput = true,
-        CreateNoWindow = true,
-    };
-    Process? shellProcess = null;
-    if (runAsAdmin) {
-        shellProcess = Process.Start(shellProcessStartInfo);
-    }
-    foreach (var item in tasks) {
-        runningTasks[i++] = Task.Run(() => {
-            // 延迟执行
-            if (item.Delay > 0) {
-                Thread.Sleep(item.Delay);
-            }
-            Logger.Debug($"Starting process {item.Name}");
-            var taskEscapeQuote = item.Args.Replace("\"", "\\\"");
-            if (runAsAdmin) {
-                // 启动任务
-                var command = $"start \"{item.Path}\"";
-                if (!string.IsNullOrEmpty(item.Args)) {
-                    command += $" -ArgumentList \"{taskEscapeQuote}\"";
-                }
-                shellProcess?.StandardInput.WriteLine(command);
-            }
-            // 非管理员身份
-            else {
-                var args = string.IsNullOrWhiteSpace(item.Args) ? string.Empty : taskEscapeQuote;
-                var arch = TaskUtils.Try(() => AppUtils.GetProgramArchitecture(item.Path));
-                var archArg = arch switch {
-                    ProgramArchitecture.X86 => "x86",
-                    _ => "amd64",
-                };
-                Process.Start(new ProcessStartInfo {
-                    FileName = "runas.exe",
-                    CreateNoWindow = true,
-                    Arguments = $"/trustlevel:0x20000 /machine:{archArg} \"{item.Path} {args}\""
-                });
-            }
-        });
-    }
-    Task.WaitAll(runningTasks);
+int i = 0;
+foreach (var item in appTasks) {
+    runningTasks[i++] = Task.Run(() => {
+        // 延迟执行
+        if (item.Delay > 0) {
+            Thread.Sleep(item.Delay);
+        }
+        Logger.Debug($"Starting process {item.Name}");
+        try {
+            Process.Start(new ProcessStartInfo {
+                FileName = item.Path,
+                Arguments = item.Args,
+                UseShellExecute = true,
+            });
+        } catch (Exception error) {
+            Logger.Error(error);
+        }
+    });
 }
+Task.WaitAll(runningTasks);
